aee16ecce70284db8110b4cdb4308fa5ca7d49cc
sgallaghma <stephen.james.gallagher@gmail.com>
2013-12-05 20:48:02 -0500

    stuff

---
+++ chap10.py
@@ -0,0 +1,113 @@
+#Steve Gallagher Chapter 11
+
+fin = open('words.txt')
+
+# fin1 = list(fin)
+
+# f = []
+
+# for x in fin1:
+# 	x = x.strip()
+# 	f.append(x)
+
+
+
+
+
+
+
+
+# #10.7
+
+def is_anagram(string1, string2):
+	a = list(string1)
+	b = list(string2)
+
+	a.sort()
+	b.sort()
+
+	n = 0
+
+	def checker(a,b,n):
+		if n < len(a) and a[n] == b[n]:           
+			n = n + 1
+			return checker(a,b,n)
+
+		elif n < len(a) and a[n] != b[n]:
+			print "No, these words are not anagrams."
+			return False
+
+		elif n >= len(a):
+			print "Yes, these words are anagrams."
+			return True
+		
+
+		
+
+	if len(a) == len(b):
+		return checker(a,b,n)
+
+
+	else:
+		print "No, these words are not anagrams."
+		return False
+	
+
+
+
+print is_anagram("reeh", "here")
+
+
+
+
+
+#10.13 interlocking words
+
+def interlock(a,b,c,n):
+	n = 0
+	for x in a:
+		c.append(a[n])
+		c.append(b[n])
+		n = n + 1
+	delimeter = ""
+	d = delimeter.join(c)
+	e = d.strip()
+	return e
+
+		
+		
+
+
+def interlock_check(string1):
+	for line in fin:
+		word = line.strip()
+
+		if len(string1) == len(word):
+			a = string1
+			b = word
+			n = 0
+			c = []
+			e = interlock(a,b,c,n)
+
+			# for y in fin:
+			# 	word1 = y.strip()
+			# 	if word1 == "schooled":
+			# 		print word1
+					# print e
+
+
+# def interlock_total():
+# 	for line in fin:
+# 		word = line.strip()
+# 		interlock_check(word)
+
+		
+print interlock_check("shoe")
+
+
+
+#interlock_total()
+
+
+
+
---
+++ chap11.py
@@ -0,0 +1,68 @@
+#Ch. 11
+# Steve Gallagher
+
+
+#11.9
+
+def histogram(li):
+	d = dict()
+	for c in li:
+		if c not in d:
+			d[c] = 1
+		else:
+			d[c] = d[c] + 1
+	return d
+
+def checker(d):
+	for x in d:
+		if d.get(x, 0) >= 2:
+			return True				
+	return False
+
+def has_duplicates(li):
+	d = histogram(li)
+	return checker(d)
+
+
+
+u = ["pie", "cake", "candy", "pie"]
+
+print has_duplicates(u)
+
+
+#11.10
+
+fin = open('words.txt')
+
+def rot_pair(string1):
+	for line in fin:
+		word = line.strip()
+		if len(word) == len(string1):
+			a = list(word)
+			a.reverse()
+			a ="".join(a)
+			if a == string1:
+				b = dict()
+				b[string1] = word
+				return b
+
+
+
+
+def rot_pair_total():
+	iterater()
+	# for line in fin:
+	# 	g = line.strip()
+	# 	print g
+	# 	# print rot_pair(g)
+		 
+	# return complete dictionary
+
+def iterater():
+	for line in fin:
+		g = line.strip
+		print rot_pair(g)
+
+# print rot_pair("evil")
+
+rot_pair_total()
